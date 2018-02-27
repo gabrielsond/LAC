a3b71f61966000c122aa0e7af4106a87b0ffe9ce
Damien Gabrielson <gabrielsond@gmail.com>
2018-02-28 00:51:00 +0900

    First commit

---
+++ LAC/FormMain.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace LAC
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.listViewFiles = new System.Windows.Forms.ListView();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxFFmpegPath = new System.Windows.Forms.TextBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listViewFiles
+            // 
+            this.listViewFiles.CheckBoxes = true;
+            this.listViewFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listViewFiles.Location = new System.Drawing.Point(12, 40);
+            this.listViewFiles.Name = "listViewFiles";
+            this.listViewFiles.Size = new System.Drawing.Size(472, 229);
+            this.listViewFiles.TabIndex = 0;
+            this.listViewFiles.UseCompatibleStateImageBehavior = false;
+            this.listViewFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(12, 275);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(472, 23);
+            this.buttonConvert.TabIndex = 1;
+            this.buttonConvert.Text = "Convert to ALAC";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_ClickAsync);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Select ffmpeg.exe";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBoxFFmpegPath.Location = new System.Drawing.Point(12, 14);
+            this.textBoxFFmpegPath.Name = "textBox1";
+            this.textBoxFFmpegPath.Size = new System.Drawing.Size(359, 20);
+            this.textBoxFFmpegPath.TabIndex = 3;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File Path";
+            this.columnHeader1.Width = 371;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Status";
+            this.columnHeader2.Width = 75;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 310);
+            this.Controls.Add(this.textBoxFFmpegPath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.listViewFiles);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "LAC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewFiles;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxFFmpegPath;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ LAC/FormMain.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAC
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(FormMain_DragEnter);
+            this.DragDrop += new DragEventHandler(FormMain_DragDrop);
+
+            string localFFmpeg = Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "ffmpeg.exe";
+
+            if (File.Exists(localFFmpeg))
+                textBoxFFmpegPath.Text = localFFmpeg;
+        }
+
+        private void FormMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void FormMain_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string filePath in filePaths)
+            {
+                if (filePath.ToLower().EndsWith(".flac"))
+                {
+                    ListViewItem foundItem = listViewFiles.FindItemWithText(filePath);
+                    if (foundItem == null)
+                    {
+                        string[] arr = new string[2];
+                        arr[0] = filePath;
+                        arr[1] = "Ready";
+                        ListViewItem listViewItem = new ListViewItem(arr);
+                        listViewItem.Checked = true;
+                        listViewFiles.Items.Add(listViewItem);
+                    }
+                } else
+                {
+                    Console.WriteLine(filePath + " does not have a FLAC extension.");
+                }
+                
+            }
+        }
+
+        private async void buttonConvert_ClickAsync(object sender, EventArgs e)
+        {
+            if(!File.Exists(textBoxFFmpegPath.Text))
+            {
+                MessageBox.Show("Please check ffmpeg.exe path.");
+                return;
+            }
+
+            buttonConvert.Enabled = false;
+            int convertCount = 0;
+
+            foreach (ListViewItem item in listViewFiles.Items)
+            {
+                if (item.Checked)
+                {
+                    item.SubItems[1].Text = "Converting...";
+                    string sourcePath = item.SubItems[0].Text;
+                    string destinationPath = Path.GetDirectoryName(sourcePath) + 
+                        Path.DirectorySeparatorChar + 
+                        Path.GetFileNameWithoutExtension(sourcePath) + 
+                        ".m4a";
+
+                    await Task.Run(() =>
+                    {
+                        convert(sourcePath, destinationPath);
+                    });
+
+                    item.SubItems[1].Text = "Done";
+                    item.Checked = false;
+                    convertCount++;
+                }
+            }
+
+            if(convertCount == 0)
+            {
+                MessageBox.Show("No files converted.");
+            } else
+            {
+                MessageBox.Show(convertCount + " files converted.");
+            }
+
+            buttonConvert.Enabled = true;
+        }
+
+        private void convert(string sourcePath, string destinationPath)
+        {
+            Process process = new Process();
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.FileName = textBoxFFmpegPath.Text;
+            process.StartInfo.Arguments = "-y -vn -i \"" + sourcePath + "\" -acodec alac \"" + destinationPath + "\"";
+            
+            process.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "FFmpeg Executable File|ffmpeg.exe";
+            openFileDialog1.Title = "Select the FFmpeg Executable";
+            openFileDialog1.FileName = "ffmpeg.exe";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // put path into box
+                textBoxFFmpegPath.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
